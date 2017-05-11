1a3839ec29f4bf8e2805155f9551c4978c36382f
Hidenobu <nishio@iMac240-3.local>
2017-05-11 22:16:57 +0900

    scss 追加

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="format-detection" content="telephone=no">
+<meta http-equiv="imagetoolbar" content="no" /> <!-- IEのイメージツールバー 無効 -->
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<title>ONESTYLE撮影のお申込み案内</title>
+<link rel="stylesheet" type="text/css" media="all" href="./style.css" />
+</head>
+
+<body>
--- index.php
+++ index.php
@@ -1,10 +1,35 @@
-<html>
-<head>
-<meta charset="utf-8">
-<title>お問い合わせ内容</title>
-</head>
-<?php
-  echo "git テストの続きの続き";
+<?php // 共通ヘッダー
+	$Path = $_SERVER['DOCUMENT_ROOT'] . '/onestyle/signature/';
+	include( $Path . 'header.php');
 ?>
-</body>
-</html>
+
+<div id="container" class="">
+	<div id="breadcrumb">
+		<ul>
+			<li><a href="<?php echo $Path; ?>">ONESTYLE撮影お申込みのご案内</a><span class="arrow">＞</span></li>
+			<li><a href="<?php echo $Path; ?>">お衣装のご予約</a></li>
+		</ul>
+	</div>
+	<!-- // #breadcrumb END -->
+
+	<div id="contents">
+		<div class="intro">
+			<h1>ONESTYLE撮影お申込みのご案内</h1>
+			<p class="b-txt">このたびはONESTYLEのフォトウェディングにお申込み頂きありがとうございます！<br class="sp-non" />今後の流れやお申込みの手続きについて、ご確認頂きますようお願い申し上げます。</p>
+			<div class="step-nav">
+				<span class="flow-current" href="">① 撮影申込書のご提出</span>
+				<a href="">② お衣装のご予約</a>
+				<a class="flow-last" href="">③ お支払いについて</a>
+			</div>
+		</div>
+		<!-- // .intro END -->
+		
+		<?php echo "git テストの続きの続き"; ?>
+		
+	</div>
+	<!-- // #contents END -->
+
+</div>
+<!-- // #container END -->
+
+<?php include( $Path . 'footer.php'); // 共通フッター ?>
