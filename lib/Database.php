3637c8c4b3312eb3a4e18a2771be985f34514ec4
vagrant <vagrant@hanmmocks-product.com>
2017-06-20 11:32:28 +0100

    printぺーじ

--- lib/Database.php
+++ lib/Database.php
@@ -5,7 +5,7 @@ class Database {
 
 
     public static function getConnection() {
-        require_once "config/db.php";
+        require_once $_SERVER['DOCUMENT_ROOT']."/config/db.php";
         $dsn = "mysql:dbname=".$db_conf["dbname"].";host=".$db_conf["host"].";charset=".$db_conf["charset"];
         $user_name = $db_conf["username"];
         $password = $db_conf["password"];
---
+++ print/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT']."/lib/Database.php";
+$db = Database::getConnection();
+$id = $_GET["id"];
+
+$stmt = $db->query("SELECT * FROM customers WHERE id=".$id);
+$data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$sp_date = explode(" ", $data["created_at"]);
+$sp_date = explode("-", $sp_date[0]);
+$do_date = $sp_date[0] . "年" . $sp_date[1] . "月" . $sp_date[2] . "日";
+
+?>
+
+<p>申し込み日<?php echo $do_date;?></p>
+<p>ご署名 新郎：<?php echo $data["groom_name"];?> 新婦：<?php echo $data["bride_name"];?></p>
+<p>ご住所：<?php echo $data["zip1"];?>-<?php echo $data["zip2"];?> <?php echo $data["prefecture"];?> <?php echo $data["address"];?></p>
+
+<?php if($data["radio_venue"]) { ?>
+<p>使用可</p>
+<?php } else { ?>
+<p>使用不可</p>
+<?php } ?>
